c6dc955fa4a158aee0c16cc36c378fc9dfb15b35
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2020-04-22 08:22:05 +0100

    Updated Uploadservice

--- src/app/uploading.service.ts
+++ src/app/uploading.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UploadingService {
-  constructor() {
+  constructor(private http: HttpClient) { }
 
+  public uploadFormData(formData) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: 'Token ' + environment.apiKey
+    });
+    return this.http.post<any>(`${environment.apiUrl}/merchant/file/`, formData, {headers});
   }
 }
